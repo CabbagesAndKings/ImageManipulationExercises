a76cd7ceb19aba63111e9a6733b055e3bc9050cf
CabbagesAndKings <eeshan@gmail.com>
2013-10-10 13:58:19 +0530

    v1 of basic manipulation

---
+++ scripts/basic manipulation.R
@@ -0,0 +1,68 @@
+
+#Basic Image Manipulation
+
+library(jpeg)
+library(reshape2)
+library(ggplot2)
+
+setwd("C:/etc/Projects/Data/_Ongoing/Image Manipulation")
+
+imageurl <- "http://farm4.staticflickr.com/3724/9416396550_2d5770cffa_b.jpg"
+download.file(imageurl, "images/baseimage.jpg", mode = "wb")
+
+LoadImageAsRGB <- function(imagefile){
+	baseimage <- readJPEG(imagefile)
+	#This is a height x width x 3(rgb) array
+	longimage <- melt(baseimage, varnames=c("y","x","rgb"), value.name="value" )
+	rgbimage <- reshape(longimage, timevar = "rgb",
+						idvar = c("x", "y"), direction = "wide")
+	colnames(rgbimage) <- c("y","x","r","g","b")
+	rgbimage$y <- -rgbimage$y #plotting axes have origin on bottom-left
+	rgbimage
+}
+
+image.base <- LoadImageAsRGB("images/baseimage.jpg") #y,x,r,g,b
+
+
+#### Plot the same image ####
+with(image.base, plot(x, y, col = rgb(r,g,b), asp = 1, pch = "."))
+#which is short for:
+plot(image.base$x, image.base$y,
+	 col = rgb(image.base$r,image.base$g,image.base$b),
+	 asp = 1, pch = ".")
+
+#Or, in ggplot2:
+g1 <- ggplot(data=image.base, aes(
+			x 	= x,
+			y 	= y,
+			col = rgb(r,g,b))
+	   ) 
+g1 + geom_point() + scale_color_identity()
+
+
+#### Add jitter/impressionism ####
+
+image.impressionist 	 <- image.base
+image.impressionist$x 	 <- jitter(image.impressionist$x)
+image.impressionist$y 	 <- jitter(image.impressionist$y)
+image.impressionist$size <- runif(1:nrow(image.impressionist), 0, 2) 
+
+with(image.impressionist, plot(x, y,
+							   col = rgb(r,g,b), asp = 1,
+							   cex = size)
+	 )
+#Or, in ggplot2:
+g2 <- ggplot(data=image.impressionist, aes(
+	x 	 = x,
+	y 	 = y,
+	size = size,
+	col  = rgb(r, g, b))
+) 
+g2 + geom_point() + scale_color_identity() +
+	theme(legend.position = "none") 
+
+
+
+
+################
+
